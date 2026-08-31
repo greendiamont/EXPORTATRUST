1349015087a42d3738d00244ba6b0f3805c20ab8
greendiamont <greendiamontzeroco2@gmail.com>
2026-08-31 14:18:38 -0300

    fix: load task position override

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./task-position-fix.css";
 import TaskAutoSaveBridge from "./task-autosave-bridge";
 
 const geistSans = Geist({
